5942efb005cb8d7ddeae45e1cd2e6302a63e8e4d
Viktoria L <113613194+Viktoria-L@users.noreply.github.com>
2023-06-05 09:07:49 +0200

    under construction

---
+++ main.js
@@ -0,0 +1,51 @@
+//education-carousel
+let swiperEdu = new Swiper(".mySwiperEducation", {
+    slidesPerView: "auto",
+    slideToScroll: 1,
+    spaceBetween: 30,
+    speed: 450,
+    loop: false,
+    mousewheel: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1100: {
+        spaceBetween: 0,
+        slidesPerView: 3,
+        direction: "vertical",
+      },
+    },
+  });
+  
+  let swiperExp = new Swiper(".mySwiperExperience", {
+    slidesPerView: "auto",
+    slideToScroll: 1,
+    spaceBetween: 30,
+    speed: 450,
+    loop: false,
+    mousewheel: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1100: {
+        spaceBetween: 0,
+        slidesPerView: 3,
+        direction: "vertical",
+      },
+    },
+  });
+  
+  //Scrollreveal
+  function revealFunction(){
+  
+    window.sr = ScrollReveal({distance:'100%', easing:'ease-out', reset:true});
+    sr.reveal('.js-reveal-0', {duration:900});
+    sr.reveal('.js-reveal-1', {duration:1000});
+    sr.reveal('.js-reveal-2', {duration:1200});
+    sr.reveal('.js-reveal-3', {duration:1400});
+  }
+  revealFunction();
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./about.html"],
+  theme: {
+    minWidth: {
+      250: "250px",
+      auto: "auto",
+    },
+    extend: {
+      spacing: {
+        105: "105%",
+      },
+      inset: {
+        80: "80%",
+        50: "50%",
+        36: "36%",
+        unset: "unset",
+      },
+      colors: {
+        my: {
+          100: "#fffffb", //vitt ish
+          200: "#eceaed", //5ljustext5
+          300: "#fec158", //4orange4
+          400: "#4D4C7D", //1ljusare bakgrund1
+          500: "#404457", //3mörkgrå 3
+          700: "#a468e3", //7ljuslila 7
+          800: "#5e6072", //6grå 6
+          900: "#363062", //2mörk bakgrund2
+        },
+      },
+      fontFamily: {
+        zilla: "Zilla Slab",
+      },
+    },
+    textShadow: {
+      23: "-13px 11px 10px rgba(0,0,0,23%)",
+      33: "-13px 11px 10px rgba(0,0,0,33%)",
+    },
+  },
+  plugins: [],
+};
